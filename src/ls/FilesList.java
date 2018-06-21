163fa4e1fa26535e5efa0dea3512c778ff76b1d5
Olga Kertling <olga.kertling@arrival.com>
2018-06-21 06:47:39 +0300

    Add check for file existance and if it is a directory

--- src/ls/FilesList.java
+++ src/ls/FilesList.java
@@ -7,23 +7,13 @@ public class FilesList {
     private File _root;
 
     public FilesList(File root) {
-        _root = root;
+        if (root != null && root.exists()) {
+            _root = root;
+        }
     }
 
     public String getList(Boolean isReverse, Boolean isLong, Boolean isHumanReadable) {
-        if (_root == null) {
-            return "";
-        }
-
-        File[] files = _root.listFiles();
-        if (files == null) {
-            return "";
-        }
-
-        List<FileInfo> result = new ArrayList<>();
-        for (File file : files) {
-            result.add(buildFileInfo(file, isLong, isHumanReadable));
-        }
+        List<FileInfo> result = listFiles(isLong, isHumanReadable);
 
         if (isReverse) {
             Collections.reverse(result);
@@ -32,6 +22,27 @@ public class FilesList {
         }
 
         return buildOutput(result);
+    }
+
+    protected List<FileInfo> listFiles(Boolean isLong, Boolean isHumanReadable) {
+        List<FileInfo> result = new ArrayList<>();
+
+        if (_root == null) {
+            return result;
+        }
+
+        if (_root.isFile()) {
+            result.add(buildFileInfo(_root, isLong, isHumanReadable));
+        } else {
+            File[] files = _root.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    result.add(buildFileInfo(file, isLong, isHumanReadable));
+                }
+            }
+        }
+
+        return result;
     }
 
     private String buildOutput(List<FileInfo> files) {
